dc1758057c42dbd5e46ffb0b6d23bab0c55a570b
Ledtobias <led.tobias@gmail.com>
2016-08-22 23:11:59 +0200

    Setting up for redesign

--- _site/assets/js/functions.js
+++ _site/assets/js/functions.js
@@ -5,7 +5,12 @@ $(window).scroll(function(){
   var wScroll = $(this).scrollTop();
 
   $('.logo').css({
-    'transform' : 'translate(0px, '+ wScroll /2.4 +'%)'
+    'transform' : 'translate(0px, '+ wScroll /1 +'%)'
+  });
+
+  $('.arrow_scrolldown').css({
+    'transform' : 'translate(0px, '+ wScroll /2 +'%)',
+    'opacity' : 1 - wScroll
   });
 
 });
