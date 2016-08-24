a24aae003f9f02ce0c153a70e1b16e900bd7b94c
Ledtobias <led.tobias@gmail.com>
2016-08-24 22:59:42 +0200

    Advanced Logo Animation on scrolling

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,16 +1,25 @@
 // Parallax Logo
 
 $(window).scroll(function(){
-
   var wScroll = $(this).scrollTop();
 
-  $('.logo').css({
-    'transform' : 'translate(0px, '+ wScroll /1 +'%)'
-  });
+  if(wScroll > $(window).height() - 200){
+    $('.logo').addClass('logoFixed').css({
+      'transform' : 'translate(0px, '+ 100% - 200 +'%)',
+      position : 'fixed'
+    })
+  } else {
+    $('.logo').removeClass('logoFixed').css({
+      'transform' : 'translate(0px, '+ wScroll /1.2 +'%)',
+      position : 'static'
+    });
+  }
+
+// Parallax Scrolldown-Arrow
 
   $('.arrow_scrolldown').css({
     'transform' : 'translate(0px, '+ wScroll /2 +'%)',
-    'opacity' : 1 - wScroll
+    'opacity' : 1 - wScroll/150
   });
 
 });
