5b053d167b67173db086a080298c84b6c656140f
Ledtobias <led.tobias@gmail.com>
2016-10-29 14:05:02 +0200

    Adding animations

---
+++ _posts/0000-05-01-alpine-sans-fontface.md
@@ -0,0 +1,9 @@
+---
+layout: post
+title: Alpine Sans Fontface
+category: Concept
+description: article ofz something important gg
+minutestoread: 20
+folder: 0000-04-01
+image: 01.jpg
+---
--- _site/assets/js/functions.js
+++ _site/assets/js/functions.js
@@ -1,1 +1,3 @@
-
+TweenMax.staggerFrom(".post", 0.4, {y:100, opacity:0, delay: 0.2}, 0.1);
+TweenMax.staggerFrom(".post em", 0.4, {x:100, rotation:90, opacity:0, delay:0.4}, 0.1)
+TweenMax.staggerFrom(".post label", 0.4, {x:100, rotation:90, opacity:0, delay:0.4}, 0.1)
